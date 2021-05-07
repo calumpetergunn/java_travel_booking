f7e5f4dfee8a31775338cc364560603268a9c2f4
calumpetergunn <calumgunn@gmail.com>
2021-05-07 13:32:00 +0100

    initial commit

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void setUp(){
+        passenger = new Passenger("Gary", 3);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Gary", passenger.getName());
+    }
+
+    @Test
+    public void canGetBags(){
+        assertEquals(3, passenger.getBags());
+    }
+
+}
